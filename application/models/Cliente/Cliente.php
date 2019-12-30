187f7be7b3bdbedbcce15dd9722aea53d9001d8b
Ricardo Alexandre <ricardo.alexandre@basis.com.br>
2019-12-30 17:28:05 -0300

    Ajustando a funcionalidade de cadastro de cliente

--- application/models/Cliente/Cliente.php
+++ application/models/Cliente/Cliente.php
@@ -3,11 +3,9 @@
 class Model_Cliente_Cliente extends Zend_Db_Table
 {
 
-    protected $_schema = 'cliente';
+    protected $_schema = 'cadastro';
 
     protected $_name = 'cliente';
 
-    protected $_primary = array(
-        'id_cliente'
-    );
+    protected $_primary = array('id_cliente');
 }
