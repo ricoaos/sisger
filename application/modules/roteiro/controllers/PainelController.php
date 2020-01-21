dea2a526e509814d41c0cba1e3c1b554105f6c73
Ricardo Alexandre <ricardo.alexandre@basis.com.br>
2020-01-21 13:47:51 -0300

    Ajustando roteiros x cliente

--- application/modules/roteiro/controllers/PainelController.php
+++ application/modules/roteiro/controllers/PainelController.php
@@ -2,13 +2,20 @@
 
 class Roteiro_PainelController extends App_Controller_Action
 {
-
+    /**
+     * 
+     * {@inheritDoc}
+     * @see Zend_Controller_Action::init()
+     */
     public function init()
     {
         // $this->idGrupo = App_Identity::getGrupo();
         $this->mCliente = new Model_Cliente_Cliente();
     }
 
+    /**
+     * 
+     */
     public function indexAction()
     {
         $select = $this->mCliente->select()
@@ -16,12 +23,17 @@ class Roteiro_PainelController extends App_Controller_Action
         ->order('st_nome ASC');
         $rsCliente = $this->mCliente->fetchAll($select)->toArray();
         $this->view->rsCliente = $rsCliente;
-        
-        
-        $mRoteiro = new Model_Roteiro_Roteiro();
-        $rsRoteiro = $mRoteiro->fetchAll()->toArray();
-        $this->view->rsRoteiro = $rsRoteiro;
     }
     
-    
+    /**
+     * 
+     */
+    public function getroteirobyclienteAction()
+    {
+        $value = $this->getRequest()->getPost('id_cliente');
+        $this->mVwClienteRoteiro = new Model_Roteiro_VwClienteRoteiro();
+        $result = $this->mVwClienteRoteiro->fetchAll(array('id_cliente = ?' => $value ))->toArray();
+        $result = json_encode(array('resultado'=>$result));        
+        $this->_helper->json($result);
+    }
 }
--- public/js/roteiro/painel.js
+++ public/js/roteiro/painel.js
@@ -1,6 +1,43 @@
 $(document).ready(function(){
 	
+	$('#grid_roteiro').hide();
+	
 	$('#id_cliente').change(function(){
-		alert($(this).val());
+				
+		$('#grid_roteiro').hide();
+		
+		$.ajax({
+			url: baseUrl+'/roteiro/painel/getroteirobycliente',
+			data: {id_cliente:$(this).val()},
+			type:"POST",
+			datatype: "json",
+				
+			success: function(result){
+				
+				var obj = JSON.parse(result); 
+				var objRoteiro = obj.resultado;
+				var campo = '';
+				
+				$.each(objRoteiro, function(i, roteiro){
+					campo += '<a href='+baseUrl+'/roteiro/'+roteiro.st_sigla+'>';
+					campo += '<button class="facebook">'+roteiro.st_sigla+'</button>';
+					campo += '</a>';
+					campo += '<span>&nbsp;&nbsp;&nbsp;&nbsp</span>'
+				});
+				
+				if(objRoteiro != '' ){
+					$('#grid_roteiro').show();
+				}
+				
+				$("#retorno").html(campo);
+			},
+			error: function(jqXHR,textStatus,errorThrown){
+	             var error = $.parseJSON(jqXHR.responseText);
+	             var content = error.content;
+	             console.log(content.message);
+	             if(content.display_exceptions)
+	             console.log(content.exception.xdebug_message);
+			}
+		});		
 	});
 });
