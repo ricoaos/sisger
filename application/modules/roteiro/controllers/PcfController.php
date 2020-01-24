c9cdab2111156889f81590ecdfb78bba6a87a8f9
Ricardo Alexandre <ricardo.alexandre@basis.com.br>
2020-01-24 17:40:04 -0300

    Atualização do banco de dados

--- application/modules/roteiro/controllers/PcfController.php
+++ application/modules/roteiro/controllers/PcfController.php
@@ -6,6 +6,7 @@ class Roteiro_PcfController extends App_Controller_Action
     public function init()
     {
         // $this->idGrupo = App_Identity::getGrupo();
+        
     }
 
     public function indexAction()
@@ -16,7 +17,7 @@ class Roteiro_PcfController extends App_Controller_Action
             $this->mCliente = new Model_Cliente_Cliente();
             $rsCliente = $this->mCliente->fetchAll(array('id_cliente = ?' => $this->_request->getParam('id') ))->toArray();
             $this->view->rsCliente = $rsCliente;
-           // Zend_Debug::dump($rsCliente);
+            //Zend_Debug::dump(App_Identity);
             //die;
         }
 
